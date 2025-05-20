82f1884d56e46173e486fa02b3f4c9a408161a2c
brentdenboer <brent@brentify.nl>
2025-05-20 23:05:26 +0200

    Removed secret example + added hcloud_ssh_key_id variable

--- terraform/main/variables.tf
+++ terraform/main/variables.tf
@@ -1,20 +1,29 @@
 variable "hcloud_token" {
+  description = "Hetzner Cloud API token"
   type        = string
   sensitive   = true
 }
 
 variable "ssh_public_key" {
+  description = "SSH public key content"
   type        = string
   sensitive   = true
 }
 
 variable "ssh_private_key" {
+  description = "SSH private key content"
+  type        = string
+  sensitive   = true
+}
+
+variable "hcloud_ssh_key_id" {
+  description = "SSH key id in Hetzner Cloud project"
   type        = string
   sensitive   = true
 }
 
 variable "firewall_ssh_source_ip" {
-  type        = string
   description = "IP address allowed to access SSH"
+  type        = string
   sensitive   = true
 }
